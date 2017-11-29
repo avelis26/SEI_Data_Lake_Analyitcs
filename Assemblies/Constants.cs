c4a8fb05df2431b5c08ea0a6555548bac98b5420
Graham Pinkston <graham.pinkston@gmail.com>
2017-11-28 23:32:03 -0600

    testing

--- Assemblies/Constants.cs
+++ Assemblies/Constants.cs
@@ -6,24 +6,9 @@ using System.Threading.Tasks;
 
 namespace Constants {
 	public class InputFileLocations {
-		public string getInputFolderPath() {
+		public static string getInputFolderPath() {
 			string date = DateTime.Now.ToString("yyyyMMdd");
 			string path = "/BIT_CRM/" + date + "/{*}";
-			return path;
-		}
-	}
-	public class OutputFileLocations {
-		string outputRoot = "/Parsed/";
-		public string getD1_120path() {
-			string path = outputRoot + "d1_120_output.csv";
-			return path;
-		}
-		public string getD1_127path() {
-			string path = outputRoot + "d1_127_output.csv";
-			return path;
-		}
-		public string getD1_135path() {
-			string path = outputRoot + "d1_135_output.csv";
 			return path;
 		}
 	}
